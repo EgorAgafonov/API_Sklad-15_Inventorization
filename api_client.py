ecd052111de0f794d6abf46fe91c2778309d45a6
Yegoriktoporik <agafonov.egor.1985@gmail.com>
2024-07-26 11:47:56 +0300

    Update files

--- api_client.py
+++ api_client.py
@@ -10,7 +10,20 @@ class Sklad15Inventorization:
     def __init__(self):
         self.base_url = url_base
 
-    def get_invent_docs_info(self):
+    def get_select_docs_info(self, query_key: str, query_value: str):
         """"""
+
+        query = {query_key: query_value}
+
+        response = requests.get(self.base_url, params=query)
+
+        status = response.status_code
+        result = ""
+        try:
+            result = response.json()
+        except json.decoder.JSONDecodeError:
+            result = response.text
+
+        return status, result
 
 
